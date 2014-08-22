0420f817461af0469eca0011ba2f3c2c2b89cafb
Alejandro Garza <alejandro.garza@acquia.com>
2014-08-22 11:00:47 -0400

    Adding files

---
+++ clone-acquia-site.sh
@@ -0,0 +1,687 @@
+#!/bin/bash
+#
+# For docs, type:
+#   bash clone-acquia-mc-site.sh -h    
+# TODOs:
+# * Allow local memcache/other cache_inc (right now it's overridden and disabled)
+# * Allow cookie_domain and base_url to remain (this script disables them)
+# * Refactor apache reload to stop and then start (in case there's not a reload command?)
+# * DOES NOT WORK WITH:
+#   * Sites with table prefixes
+# * enabling logging email to file using devel?
+# * Add $_ENV['AH_SITE_ENVIRONMENT'] = 'local'; ??
+
+# Get the path to this script
+clone_command_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -r $clone_command_folder/config.sh ]
+then
+  cat <<EOF
+No $clone_command_folder/config.sh file found!
+* You should copy the included $clone_command_folder/config.sh.default file into
+a config.sh file.
+* Then, you must edit this file to match your environment.
+EOF
+  exit 1
+else
+  source $clone_command_folder/config.sh
+fi
+
+# Include common functions
+source $clone_command_folder/functions.sh
+
+# Defaults
+sitefolder=default #Site folder you are targetting.
+
+# BEGIN!
+# Get options
+# http://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options/7680682#7680682
+while test $# -gt 0
+do
+  case $1 in
+
+  # Normal option processing
+    -h | --help)
+      HELP=1
+      ;;
+
+  # Special cases
+    --)
+      break
+      ;;
+  # Long options
+    --help)
+      HELP=1
+      ;;
+    --uri=*)
+      URI=$1
+      ;;
+    --mc)
+      STAGE=$1
+      ;;
+    --dc)
+      STAGE=$1
+      ;;
+    --network)
+      STAGE=$1
+      ;;
+    --skip-db)
+      echo "  Skipping database download/creation"
+      skip_db=1
+      ;;
+    --skip-repo)
+      echo "  Skipping repository checkout"
+      skip_repo=1
+      ;;
+    --skip-convert-myisam)
+      echo "  Skipping InnoDB -> MyISAM conversion"
+      innodb_to_myisam=0
+      ;;
+    --site-folder=*)
+      sitefolder=`echo $1 |cut -f2- -d=`
+      echo "  Using sitefolder $sitefolder"
+      ;;
+    --site-db=*)
+      ac_db_name=`echo $1 |cut -f2- -d=`
+      echo "  Using remote Database $ac_db_name"
+      ;;
+    --local-hostname=*)
+      local_hostname=`echo $1 |cut -f2- -d=`
+      echo "  Using localhostname $local_hostname"
+      ;;
+    --local-dbname=*)
+      local_dbname=`echo $1 |cut -f2- -d=`
+      echo "  Using localdbname $local_dbname"
+      ;;
+    --local-dbfile=*)
+      local_dbfile=`echo $1 |cut -f2- -d=`
+      echo "  Using localdbfile $local_dbfile"
+      ;;
+    --delete)
+      delete_site=1;
+      ;;
+    --*)
+      # error unknown (long) option $1
+      echo "  Warning: Unknown option $1"
+      ;;
+    -?)
+      # error unknown (short) option $1
+      ;;
+
+  # MORE FUN STUFF HERE:
+  # Split apart combined short options
+  #  -*)
+  #    split=$1
+  #    shift
+  #    set -- $(echo "$split" | cut -c 2- | sed 's/./-& /g') "$@"
+  #    continue
+  #    ;;
+
+  # Done with options. Remaining arguments are now in $1, $2 etc.
+    *)
+      break
+      ;;
+  esac
+
+  shift
+done
+
+
+# Check the arguments!
+sitename=${1:-x}
+env=${2:-x}
+ac_db_name=${ac_db_name:-${sitename}}
+if [ $sitename = x -o $env = x -o ${HELP:-x} = 1 ]
+then
+  cat <<EOF
+USAGE: $0 sitename env
+
+Clones a site locally. Usage:
+  clone-acquia-mc-site.sh [options] sitename env
+Options:
+  -h or --help          : Shows this help text and exits.
+  --mc  or  --dc        : Force devcloud/managed cloud site.
+  --skip-repo           : Skip repo checkout
+  --skip-db             : Skip DB download/creation
+  --site-folder=default : name of folder within docroot/sites/* to use. Defaults to 'default'
+  --site-db=sitename    : Database name to use. Use same string as from the Acquia require line:
+                            require('/var/www/site-php/[sitename]/[sitedb]-settings.inc');
+                         (Defaults to the same value as the sitename argument.)
+  --local-hostname=...  : Local hostname to use. Defaults to "local.[env].[sitename]"
+  --local-dbname=...    : Local DB name to use.  Defaults to "local.[env].[sitename].[site-folder]"
+  --local-dbfile=...    : Local DB file to use. If present, it skips downloading the DB from the site.
+  --skip-convert-myisam : Do *NOT* convert all DB tables to MyISAM (for perf. purposes)
+  --delete              : DELETE a local site completely *** DANGER ***
+EOF
+  exit
+fi
+
+# Some calculated vars
+hostname=${local_hostname:-local.${env}.${sitename}}
+dbname=${local_dbname:-${hostname}.${ac_db_name}}
+dest_dir_site=${dest_dir}/${hostname}
+tmpscript=/tmp/tmp$$.sh
+tmpscript2=/tmp/tmp$$.2.sh
+tmpout=/tmp/tmp$$.out
+datetime=`date +"%D %T"`
+phpstorm_idea_template_folder=$clone_command_folder/phpstorm-idea-template-folder
+vhosts_includes_dir=$dest_dir/vhosts-config-apache #folder to place generated [hostname].conf files included from apache's vhosts config.
+
+# Are we DELETING??
+# TODO: Delete from /var/www/site-php/*.inc
+if [ ${delete_site:-x} = 1 ]
+then
+  echo "Deleting site:"
+  echo "       Folder: $dest_dir"
+  echo "     Database: $dbname"
+  echo "  Hosts entry: $hostname"
+  echo
+  echo "** PRESS ANY KEY TO CONTINUE, CTRL-C TO BREAK **"
+  read
+  
+  # /etc/hosts
+  if [ `egrep -c "^(# Entry for |127.0.0.1 )$hostname" /etc/hosts` -gt 0 ]
+  then
+    echo "Removing hosts entry $hostname ..."
+    hosts_bak=/tmp/hosts.backup.$$
+    cat /etc/hosts >$hosts_bak
+    echo "  Made backup at $hosts_bak"
+    egrep -v "^(# Entry for |127.0.0.1 )$hostname" $hosts_bak |sudo tee /etc/hosts >/dev/null
+    echo "  Wrote /etc/hosts"
+  else
+    echo "Warning: No entries for $hostname in /etc/hosts!"
+  fi
+  echo ""
+  
+  # Database
+  if [ `echo SHOW DATABASES |mysql |grep -c $dbname` -eq 1 ]
+  then
+    echo "Removing DB $dbname ..."
+    mysqladmin drop $dbname
+    echo "  Done."
+  else
+    echo "Warning: Database $dbname does not exist!"
+  fi
+  echo ""
+  
+  # Vhosts entry
+  if [ -r ${vhosts_includes_dir}/$hostname.conf ]
+  then
+    echo "Removing ${vhosts_includes_dir}/$hostname.conf"
+    rm ${vhosts_includes_dir}/$hostname.conf
+    echo "  Done."
+  else
+    echo "Warning: No vhosts entry ${vhosts_includes_dir}/$hostname.conf"
+  fi
+  echo ""
+  
+  # /var/www/site-php/XXX
+  if [ -r /var/www/site-php/$sitename ]
+  then
+    echo "Removing /var/www/site-php/$sitename"
+    sudo rm /var/www/site-php/$sitename
+    echo "  Done."
+  else
+    echo "Warning: No symlink /var/www/site-php/$sitename found"
+  fi
+  echo ""
+  
+  # Site folder
+  if [ -d $dest_dir_site ]
+  then
+    echo "Removing site folder at $dest_dir_site ..."
+    rm -rf $dest_dir_site
+    echo "  Done."
+  else
+    echo "Warning: Could not find folder at $dest_dir_site!"
+  fi
+  echo ""
+
+  echo "Finished!"
+  exit 0
+fi
+
+#
+# Run a few checks first
+#
+cd $dest_dir
+if [ $? -gt 0 ]
+then
+  echo "ERROR: Could not find destination folder (defined in \$dest_dir):"
+  echo "   $dest_dir"
+  exit 1
+fi
+
+# Make sure required files/paths exist
+if [ ! -r /var/www/site-php ]
+then
+  echo "ERROR: Could not find the required /var/www/site-php folder"
+  echo "  You should create it first by running:"
+  echo "  mkdir -p /var/www/site-php"
+  exit 1
+fi
+if [ ! -r $vhosts_path ]
+then
+  echo "ERROR: Could not find your apache vhosts configuration file (defined in \$vhosts_path):"
+  echo "  $vhosts_path"
+  exit 1
+fi
+if [ ! -r $vhosts_includes_dir ]
+then
+  echo "ERROR: Could not find \$vhosts_includes_dir: ${vhosts_includes_dir}"
+  echo "  If the path is correct, you should create it with:  mkdir -p ${vhosts_includes_dir}"
+  echo "  Or, edit the path to use on the script."
+  exit 1
+fi
+if [ ! -w $vhosts_includes_dir ]
+then
+  echo "ERROR: \$vhosts_includes_dir: ${vhosts_includes_dir} is not writeable."
+  exit 1
+fi
+if [ `grep -c "Include ${vhosts_includes_dir}/*" $vhosts_path` -eq 0 ]
+then
+  echo "ERROR: ${vhosts_path} does not have an include line pointing to ${vhosts_includes_dir}"
+  echo "   Please add this line to the file:"
+  echo ""
+  echo "   Include ${vhosts_includes_dir}/*"
+  exit 1
+fi
+if [ ! -x $drush ]
+then
+  echo "ERROR: Could not find drush at the path specified in \$drush:"
+  echo "  $drush"
+  exit 1
+fi
+
+# Check database does not exist
+if [ `echo "SHOW DATABASES LIKE '$dbname'" | mysql -u$dbuser --password=$dbpassword |wc -l` -gt 0 -a ${skip_db:-0} = 0 -a ${local_dbfile:-0} = 0 ]
+then
+  echo "ERROR: Database $dbname already exists. You can remove it, or use the --skip-db option."
+  echo "  To remove it, run:"
+  echo "  mysqladmin -u$dbuser --password=$dbpassword drop $dbname"
+  exit 1
+fi
+
+if [ ${local_dbfile:-x} != x ]
+then
+  if [ ! -r $local_dbfile ]
+  then
+    echo "ERROR: --local-dbfile=$local_dbfile doesn't exist"
+    echo ""
+    exit 1
+  fi
+fi
+
+# Check that needed commands exist and are executable.
+path_ok=1
+#   Loop thru commands
+for command in mysql pv
+do
+  which $command >/dev/null 2>&1
+  if [ $? -gt 0 ]
+  then
+    echo "ERROR: the '${command}' command is not in your PATH enviroment variable."
+    path_ok=0
+  fi
+done
+#   Quit if any command wasn't found
+if [ $path_ok -eq 0 ]
+then
+  echo ""
+  echo "Current value of PATH:"
+  echo "  $PATH"
+  echo ""
+  exit 1
+fi
+
+# ERROR when /var/www/site-php/${sitename} already exists
+if [ -r /var/www/site-php/${sitename} ]
+then
+  echo "ERROR: /var/www/site-php/${sitename} exists and points here:"
+  ls -l /var/www/site-php/${sitename} 
+  exit 1
+fi
+
+
+# WARN when destination repofolder already exists
+if [ -r $dest_dir_site ]
+then
+  echo "WARNING: Destination folder $dest_dir_site already exists!"
+  echo "You might want to try going an already-existing local site here:"
+  echo "  http://${hostname}/"
+fi
+
+if [ ${sitefolder:-x} = x ]
+then
+  # Check sitename/env exists!
+  ahtaht sites >$tmpout
+  if [ $? -gt 0 ]
+  then
+    echo "ERROR: aht could not find the site/environment using: aht @${sitename}.${env}"
+    exit 1
+  fi
+
+  # Warning if this looks like a multisite Drupal site.
+  if [ `grep -c . $tmpout` -gt 1 ]
+  then
+    echo "WARNING: this site has various sites/* folders, but you specified none with the --sitefolder=xxx option."
+    echo "  ${sitename}.${env} currently has these sites:"
+    cat $tmpout |awk '{ print "    " $0 }'
+    echo ""
+    read -p "HIT [ENTER] TO CONTINUE; THE SCRIPT WILL ASSUME THE 'default' SITE FOLDER" foo
+  fi
+fi
+
+#
+# Start!
+#
+mkdir $dest_dir_site 2>/dev/null
+cd $dest_dir_site
+
+# Make a file that shows what the arguments used to call this script were.
+date=`date`
+cat <<EOF >clone-site-args.txt
+# These are the arguments used to call the clone-acquia-mc-site.sh script
+# Generated on $date 
+URI=$URI
+STAGE=$STAGE
+sitefolder=$sitefolder
+ac_db_name=$ac_db_name
+local_hostname=$local_hostname
+local_dbname=$local_dbname
+local_dbfile=$local_dbfile
+EOF
+
+# Clone the repository
+if [ ${skip_repo:-x} = x ]
+then
+  echo "Attempting to checkout the code repository..."
+  # Note, sed https://... to fix broken aht repo --checkout
+  ahtaht repo --checkout |grep -v "Site is in live development"|tr -d '\012\015' |sed -e 's/https:..https:../https:\/\//'  > $tmpscript
+  # Check command successful
+  if [ $? -gt 0 ]
+  then
+    exit 1
+  fi
+  # Check NOT in livedev
+  #if [ `grep -c "Site is in live development" $tmpscript` -eq 1 ]
+  #then
+  #  echo "Error:"
+  #  cat $tmpscript
+  #  exit 1
+  #fi
+  sep="/"
+  if [ `cat $tmpscript |grep -c .git` -eq 1 ]
+  then
+    sep=":"
+  fi
+  repofolder=`cat $tmpscript | cut -f2 -d'@' |awk -F${sep} '{ print $NF }' |cut -f1 -d' ' |sed -e 's/.git$//'`
+  # Check repofolder does not exist
+  if [ -r $repofolder ]
+  then
+    echo "  $repofolder exists! Skipping cloning..."
+  else 
+    # Clone it
+    echo "Cloning the code repository to $repofolder, using:"
+    cat $tmpscript | awk '{ print "  " $0 }'
+    sh $tmpscript
+    if [ $? -gt 0 ]
+    then
+      echo "Error cloning code repository to $repofolder."
+      exit 1
+    fi
+  fi
+  echo "Done!"
+  echo ""
+else
+  echo "Skipping repo clone. Looking for existing repository..."
+  repofolder=`find . -maxdepth 2 -name docroot |cut -f2 -d'/'`
+  if [ ${repofolder:-x} != x ]
+  then
+    echo "Repository found at $repofolder"
+  else
+    echo "ERROR: Could not find a repo folder with a 'docroot' folder within "`pwd`
+    exit 1
+  fi
+  echo ""
+fi
+
+#
+# Get DB and load it locally.
+#
+if [ ${skip_db:-x} = x ]
+then
+  if [ ${local_dbfile:-x} = x ]
+  then
+    echo "Getting database..."
+    ahtaht db-backups-get --latest --database=$ac_db_name >$tmpscript
+    if [ $? -gt 0 ]
+    then
+      echo "ERROR: Could not find database '$ac_db_name'"
+      echo "  Please specify a correct one using --sitedb=[dbname]"
+      exit 1
+    fi
+    cat $tmpscript |tr -d '\015' >$tmpscript2
+    dbfilename_remote=`cat $tmpscript2 |awk '{ print $(NF-1); }'`
+    dbfilename=`basename ${dbfilename_remote}`
+    
+    # The above should set $dbfilename and $tmpscript
+    
+    echo "  DB filename: $dbfilename"
+    echo "  Rsync script from $tmpscript:"
+    cat $tmpscript2 | awk '{ print "    " $0 }'
+    
+    # Sync it!!
+    echo "  ryncing the DB file..."
+    bash $tmpscript2
+    # Confirm the file made it!
+    if [ ! -r $dbfilename ]
+    then
+      echo "ERROR: Could not find synced DB file: $dbfilename in "`pwd`
+      exit 1
+    fi
+    echo "Done!"
+    echo ""
+  else
+    echo "Using local DB file: $local_dbfile"
+    dbfilename=$local_dbfile
+  fi
+
+  #
+  # Create a local DB
+  #
+  echo "Creating the database at $dbname from file $dbfilename"
+  mysqladmin -u$dbuser --password=$dbpassword create $dbname 2>/dev/null
+  if [ $? -gt 0 ]
+  then
+    echo "Error! Could not create database $dbname"
+    exit 1
+  fi
+  # Import the database, skipping some data:
+  echo "  Starting DB import..."
+  pv -p $dbfilename |gzip -d -c | awk -F'`' '
+NR==1 { 
+  # http://superuser.com/questions/246784/how-to-tune-mysql-for-restoration-from-mysql-dump
+  # TODO? http://www.palominodb.com/blog/2011/08/02/mydumper-myloader-fast-backup-and-restore ?
+  print "SET SQL_LOG_BIN=0;"
+  print "SET unique_checks=0;"
+  print "SET autocommit=0;"
+  print "SET foreign_key_checks=0;"
+  output=1;
+} 
+{ 
+  start_of_line=substr($0,1,200);
+  # Detect beginning of table structure definition.
+  if (index(start_of_line, "-- Table structure for table")==1) {
+    output=1
+    print "COMMIT;"
+    print "SET autocommit=0;"
+    current_db=$2    ## before, it was start_of_line
+  }
+  # Switch the engine from InnoDB to MyISAM : MUCHO FAST. 
+  if (substr(start_of_line,1,8)==") ENGINE" && '${innodb_to_myisam:-0}' == 1) {
+    if (current_db ~ /^(locales_source|locales_target|menu_links|workbench_scheduler_types)/) {
+      print "Skipping InnoDB -> MyISAM for " current_db >"/dev/stderr"
+    } else {
+      gsub(/=InnoDB/, "=MyISAM", $0);
+      gsub(/CHARSET=utf8/, "CHARSET=latin1", $0);
+    }
+  }
+  # Detect beginning of table data dump.
+  if (index(start_of_line, "-- Dumping data for table")==1) {
+    if (current_db != $2) {
+      print "Internal problem: unexpected data, seems to come from table " $2 " whereas expected table " current_db;
+      current_db=$2
+    }
+    printf "\r Processing table " current_db > "/dev/stderr"
+    output=1
+    # Skip data in some tables
+    if (current_db ~ /^(__ACQUIA_MONITORING|accesslog|batch|cache|cache_.*|history|queue|search_index|search_dataset|search_total|sessions|watchdog|boost_cache|panels_hash_database_cache|migrate_.*)$/) {
+      output=0
+      print "... Skipping Data import (imported structure only) for " current_db >"/dev/stderr"
+    }
+  }
+  if (output==1) {
+    print
+  }
+}
+END {
+  print "COMMIT;"
+}' |mysql -u$dbuser --password=$dbpassword $dbname
+
+  if [ $? -gt 0 ]
+  then
+    echo "Error! Could not import data into database $dbname"
+    echo "  If you got a 'Key too long' error, try running again using the --skip-convert-myisam flag"
+    exit 1
+  fi
+  
+  echo "  Importing done!"
+  
+  # Scrub the users in the DB
+  echo "  Scrubbing the users table in the DB..."
+  echo "UPDATE users SET mail=CONCAT('user', uid, '@example.com') WHERE uid > 0" |mysql -u$dbuser --password=$dbpassword $dbname
+  echo "  Scrubbing done!"
+  
+  echo "Done!"
+  echo ""
+else
+  echo "Skipping DB download/import. Using database: $dbname"
+  echo ""
+fi
+
+# Get some variables
+docroot=`pwd`"/$repofolder/docroot"
+sitefolderpath="${docroot}/sites/${sitefolder}"
+if [ ! -r $sitefolderpath ]
+then
+  echo "ERROR: Could not find the '${sitefolder}' site folder at $sitefolderpath"
+  exit 1
+fi
+
+
+add_varwwwsitephp $dest_dir_site $docroot $dbname $hostname $sitefolder $dbuser $dbpassword $sitename $ac_db_name 
+
+add_vhosts $vhosts_includes_dir $dest_dir_site $docroot $hostname
+
+# Create and allow writing to new php-errors.log
+echo "Creating empty .log files at ${dest_dir_site}..."
+touch ${dest_dir_site}/error.log
+touch ${dest_dir_site}/access.log
+touch ${dest_dir_site}/php-errors.log
+chmod a+w ${dest_dir_site}/*.log
+ls ${dest_dir_site}/*.log | awk '{ print "  " $0 }'
+echo "Done!"
+echo ""
+
+add_etchosts $hostname
+
+#
+# Create files folder!
+#
+echo "Creating EMPTY files folder at $sitefolderpath/files"
+mkdir $sitefolderpath/files 2>/dev/null 
+chmod a+w $sitefolderpath/files
+echo "Done!"
+echo ""
+
+#
+# Restart apache!
+#
+echo "Restarting apache"
+sudo $reload_apache_command
+echo "Done!"
+echo ""
+
+#
+# Test that Drush runs.
+#
+echo "Reconfiguring site using drush"
+cd $sitefolderpath
+$drush status >/dev/null 2>&1
+if [ $? -gt 0 ]
+then
+  echo "  WARNING: Drush failed to run!"
+else 
+  #
+  # Issue some drush commands
+  #
+  modules_to_disable="memcache memcache_admin securelogin shield securepages"
+  echo "  Disabling some modules: $modules_to_disable"
+  $drush dis -y $modules_to_disable
+  echo "  Activating user 1 in case it's disabled"
+  echo "UPDATE users SET status=1 WHERE uid=1" | $drush sql-cli
+  echo "  Done!"
+fi
+echo ""
+
+#
+# Add in a pre-made PHPStorm project
+#
+if [ -r $phpstorm_idea_template_folder ]
+then
+  echo "Adding PHPStorm project for this site..."
+  cp -R $phpstorm_idea_template_folder $docroot/.idea
+  # Change some variables in the project files
+  if [ `grep -c "core *= *7.x" $docroot/modules/node/node.info` -eq 1 ]
+  then
+    DRUPAL_VERSION=7
+  else
+    DRUPAL_VERSION=6
+  fi
+  cd $docroot/.idea
+  cat workspace.xml |sed -e "s/{{DRUPAL_VERSION}}/$DRUPAL_VERSION/" >$tmpout && cp $tmpout workspace.xml
+  cat deployment.xml | sed -e "s%{{SITE_URL}}%$hostname%" >$tmpout && cp $tmpout deployment.xml
+  echo "  Done!"
+  echo "  You can open the project directly in PhpStorm by running:"
+  echo "    storm \"$docroot\""
+  echo ""
+fi
+
+cd $docroot
+
+# Done!
+echo "Site ready!!!! \o/"
+echo ""
+  
+#
+# Bonus points:
+# Get ULI location
+#
+uli=`$drush uli --uri=$hostname`
+if [ $? -eq 0 ]
+then
+  echo "You can go to the admin account here:"
+  echo "  $uli"
+else
+  echo "Could not get one-time login link via drush :("
+fi
+echo "Site located here:"
+echo "   On disk: $dest_dir_site"
+echo "  Via http: http://$hostname/"
+echo ""
+
+#
+# Cleanup
+#
+rm $tmpscript $tmpscript2 $tmpout 2>/dev/null
---
+++ functions.sh
@@ -0,0 +1,182 @@
+# Wrapper for aht
+function ahtaht() {
+  #echo "** Aht command:    aht $STAGE @$SITENAME $URI $@" 1>&2
+  aht $STAGE @${sitename}.${env} $@
+}
+
+#
+# Add entry for vhosts
+#
+function add_vhosts() {
+  vhosts_includes_dir=$1
+  dest_dir_site=$2
+  docroot=$3
+  hostname=$4
+  
+  datetime=`date +"%D %T"`
+  
+  echo "Adding entry for apache vhosts at $vhosts_includes_dir/$hostname.conf ..."
+  if [ ! -r $vhosts_includes_dir/$hostname.conf ]
+  then
+    echo "  Adding vhosts entry: $hostname => $dest_dir_site"
+    #cat <<EOF |tee -a $vhosts_includes_dir/$hostname.conf
+    cat <<EOF >> $vhosts_includes_dir/$hostname.conf
+# Entry for $hostname, added $datetime
+<VirtualHost *:80>
+    DocumentRoot $docroot
+    ServerName $hostname
+    ErrorLog "${dest_dir_site}/error.log"
+    CustomLog "${dest_dir_site}/access.log" combinedio
+    php_value error_log ${dest_dir_site}/php-errors.log
+</VirtualHost>
+
+# SSL Version. Same except for last 3 lines starting with SSL*
+<VirtualHost *:443>
+    DocumentRoot $docroot
+    ServerName $hostname
+    ErrorLog "${dest_dir_site}/error.log"
+    CustomLog "${dest_dir_site}/access.log" combinedio
+    php_value error_log ${dest_dir_site}/php-errors.log
+    # Here come the SSL config lines :)
+    SSLEngine on
+    SSLCertificateFile "/opt/lampp/etc/ssl.crt/server.crt"
+    SSLCertificateKeyFile "/opt/lampp/etc/ssl.key/server.key"
+</VirtualHost>
+
+EOF
+  else
+    echo "  WARNING: Apache vhosts entry for $hostname already exists."
+  fi
+  echo "Done!"
+  echo ""
+}
+
+#
+# Add entry to /etc/hosts
+#
+function add_etchosts() {
+  hostname=$1
+  ip=${2:-127.0.0.1}
+  datetime=`date +"%D %T"`
+  echo "Adding entry for $hostname to /etc/hosts"
+  if [ `grep -c "$hostname" /etc/hosts` -eq 0 ]
+  then
+    cat <<EOF |sudo tee -a /etc/hosts
+# Entry for $hostname, added $datetime
+$ip $hostname
+EOF
+  else
+    echo "  WARNING: Entry for $hostname already exists on /etc/hosts:"
+    #grep -A 1 "# Entry for $hostname, added.*" /etc/hosts |awk '{ print "  " $0 }'
+    grep -B 1 $hostname /etc/hosts |awk '{ print "  " $0 }'
+  fi
+  echo "Done!"
+  echo ""
+}
+
+#
+# Add connection string to Drupal
+#
+function add_varwwwsitephp() {
+  dest_dir_site=$1 #full path to repo folder
+  docroot=$2
+  dbname=$3        #local mysql dbname
+  hostname=$4      #hostname
+  sitefolder=${5:-default} #folder within sites/[sitefolder]
+  dbuser=${6:-root}
+  dbpassword=${7}
+
+  tmpout=/tmp/tmp$$.1
+  settings=$docroot/sites/$sitefolder/settings.php
+  
+  if [ `grep -c "['\"].var.www.site-php.*['\"]" $settings` -eq 1 ]
+  then
+    #Extract the sitename and ac_db_name from the existing require line
+    grep -o "['\"].var.www.site-php.*['\"]" $settings |awk -F/ '{ print "ac_db_name=" substr($6, 1, index($6, "-")-1); print "sitename=" $5 }' >$tmpout
+    cat $tmpout
+    . $tmpout
+  else
+    sitename=$8      #@[this].env, e.g. 'eeagarza'
+    ac_db_name=$9    #db name in acquia cloud, e.g. 'eeagarza'
+  fi
+    
+  datetime=`date +"%D %T"`
+  if [ ! -r /var/www/site-php/${sitename} ]
+  then
+    echo "Linking /var/www/site-php/${sitename} to $dest_dir_site"
+    sudo ln -s ${dest_dir_site} /var/www/site-php/$sitename
+  fi
+  echo "Adding DB connection string and other overrides to $dest_dir_site/${ac_db_name}-settings.inc"
+  cat <<EOF >$dest_dir_site/${ac_db_name}-settings.inc
+<?php
+
+/**
+ * Created by clone-acquia-mc-site.sh
+ * For host: $hostname
+ *       on: $datetime
+ */
+
+// Connection to local DB.
+// D7 Version
+\$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => '$dbname',
+  'username' => '$dbuser',
+  'password' => '$dbpassword',
+  'host' => 'localhost',
+  'port' => 3306,
+  #'prefix' => 'main_', #TODO
+  'collation' => 'utf8_general_ci',
+);
+// D6 Version
+\$db_url='mysqli://$dbuser:$dbpassword@localhost/$dbname';
+
+# Force some variables so as not to wreak havok on real site!
+# Force apachesolr to be read-only!
+\$conf['apachesolr_read_only'] = 1;
+# Drupal should not run cron automatically
+\$conf['cron_safe_threshold'] = 0;
+
+# Add more memory, just in case!
+ini_set('memory_limit', '256M');
+
+# Fix file paths
+\$conf['file_public_path'] = 'sites/$sitefolder/files';
+\$conf['file_private_path'] = 'sites/$sitefolder/files';
+\$conf['file_temporary_path'] = '/tmp';
+
+# Fix for local domain.
+\$base_url = "http://$hostname";
+\$cookie_domain = "$hostname";
+
+# Report all PHP errors
+error_reporting(E_ALL);
+# Show errors on the HTML output sent to browsers.
+ini_set('display_errors', TRUE);
+# Show any errors that happen during PHP startup
+ini_set('display_startup_errors', TRUE);
+# Force-override the Drupal variable for error levels to show, to show "All messages"
+# Setting is at: Administration > Configuration > Development > logging and errors
+# Value 2 == ERROR_REPORTING_DISPLAY_ALL
+\$conf['error_level'] = 2;
+
+####################
+# Memcache stuff::
+# - Set the key to something sane for your local memcache.
+\$conf['memcache_key_prefix'] = '${hostname}_';
+
+# To DISABLE memcahe on a site that already has it, uncomment the following:
+#unset(\$conf['cache_inc']);
+
+# To ENABLE memcache on a site that does not already has it enabled, uncomment this:
+#\$conf['cache_backends'][] = './sites/all/modules/contrib/memcache/memcache.inc';
+#\$conf['cache_default_class'] = 'MemCacheDrupal';
+#\$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+#####################
+
+////////////////////////////////////////////////////////////////
+
+EOF
+  echo "Done!"
+  echo ""
+}
